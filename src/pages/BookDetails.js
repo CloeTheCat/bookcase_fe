0a7daa848fd91a49947bdadcb042c0e5d85f81db
CloeTheCat <claudia.pusceddu@outlook.it>
2024-02-02 13:44:21 +0100

    gets data for book details working, install moment

--- src/pages/BookDetails.js
+++ src/pages/BookDetails.js
@@ -2,20 +2,52 @@ import { Button, IconButton, Card } from '@mui/material';
 import { Remove, Add, ArrowBack } from '@mui/icons-material';
 
 import classes from './BookDetails.module.css';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getBookData } from '../services/getBookData';
+import { useSelector } from 'react-redux';
+import moment from 'moment';
 
+let dummyBook = {
+  "id_book": 0,
+  "title": "Titolo",
+  "author": "Autore",
+  "isbn": 0,
+  "plot": "Trama",
+  "id_userlibrary": null,
+  "id_user": null,
+  "added_on": null,
+  "removed_on": null,
+  "read_count": 0
+}
 
-function BookDetails({ bookData }) {
+function BookDetails() {
+  const userDetails = useSelector(state => state.login.user);
+  const [bookData, setBookData] = useState(dummyBook);
+
+  let { id_book } = useParams();
+
+  useEffect(() => {
+    const id_user = userDetails.id_user;
+    const bookId = parseInt(id_book)
+    userDetails && getBookData({ id_book: bookId, id_user: id_user })
+      .then((res) => {
+        setBookData(res.data)
+        console.log('res', res)
+      })
+  }, [])
 
   let asideContent = <>
     <Button variant='contained' sx={buttonStyle}>Aggiungi alla libreria</Button>
   </>;
 
-  if (bookData.addedOn !== null) {
+  if (bookData.added_on !== null && bookData.added_on !== undefined) {
+    let added_on = moment(bookData.added_on).format('D MM YYYY');
+
     asideContent = <>
       <Card sx={cardStyle}>
-        <p className={classes.userDetailsText}>Aggiunto alla tua libreria il {bookData.addedOn}</p>
-        <p className={classes.userDetailsText}>Hai letto questo libro {bookData.readCount} volte.</p>
+        <p className={classes.userDetailsText}>Aggiunto alla tua libreria il {added_on}</p>
+        <p className={classes.userDetailsText}>Hai letto questo libro {bookData.read_count} volte.</p>
         <IconButton aria-label='meno'><Remove /></IconButton>
         <IconButton aria-label='più'><Add /></IconButton>
       </Card>
@@ -33,7 +65,7 @@ function BookDetails({ bookData }) {
         Torna alla tua libreria
       </Button>
     </Link>
-    <div className={classes.detailsWrapper}>
+    {bookData !== null && <div className={classes.detailsWrapper}>
       <article className={classes.bookDetails}>
         <h1 className={classes.bookTitle}>{bookData.title}</h1>
         <h2 className={classes.bookAuthor}>di {bookData.author}</h2>
@@ -44,7 +76,7 @@ function BookDetails({ bookData }) {
       <aside className={classes.userDetails}>
         {asideContent}
       </aside>
-    </div>
+    </div>}
   </>
 
   );
